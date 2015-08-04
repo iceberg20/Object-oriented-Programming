9fc80fb141c2c34d1aa527b49e1563d47ddc3a37
Italo Berg <italoberg@yahoo.com.br>
2015-08-04 00:31:03 -0300

    Command Line menu created
    -Classe Diagram Update - new method added at contact classe
    -A command line menu was added

--- Ruby Code/AdressBook.rb
+++ Ruby Code/AdressBook.rb
@@ -38,7 +38,7 @@ class Contact
 		@adr.showAdress
 	end
 end
-
+=begin
 firstAdress = Adress.new("Av. Ayrton Senna",2023, 59151902)
 secondAdress = Adress.new("Av. Salgado Filho",5050, 599000)
 
@@ -51,4 +51,33 @@ adBook = AdressBook.new
 adBook.addContact(c1)
 adBook.addContact(c2)
 adBook.showAdresses
+=end
+puts "###################################"
+puts "### Welcome to the AdressBook! ####"
+puts "### Developed by Ítalo Berg #######"
+puts "###################################"
+puts
+puts
 
+op = 10
+while op!=0 do
+	puts "Menu 1- Add Contact 2- Show Contacts"
+	op = gets.to_i	
+	if op == 1 then
+		puts "Type the name:"
+		name = gets
+		puts "Type the phone number"
+		phone = gets
+		puts "Type the street"
+		street = gets
+		puts "Type the adress number"
+		adrNumber = gets
+		puts "Type the postal code"
+		postal = gets
+
+		adr = Adress.new(street,adrNumber, postal)
+
+		c1 = Contact.new(name,phone, adr)
+	end
+
+end
